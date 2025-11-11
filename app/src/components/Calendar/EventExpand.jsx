740e1a99b59b08fa3ec5928f5fc9bb8822c3a3dd
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-11-11 01:50:26 +0100

    Updated Calendae

--- app/src/components/Calendar/EventExpand.jsx
+++ app/src/components/Calendar/EventExpand.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import styles from "./Calendar.module.css";
 
+import Icon from "../Icon";
+
 const EventExpand = ({ isExpandable, isExpanded }) => {
   if (!isExpandable) return null;
 
@@ -8,10 +10,10 @@ const EventExpand = ({ isExpandable, isExpanded }) => {
     <div className={styles.expand}>
       <motion.div
         className={styles.expand_inner}
-        animate={{ rotate: isExpanded ? 45 : 0, y: -1.2 }}
+        animate={{ rotate: isExpanded ? 45 : 0, y: 0 }}
         transition={{ duration: 0.3, ease: "easeInOut" }}
       >
-        +
+        <Icon className={styles.icon} path="/assets/icons/plus.svg" />
       </motion.div>
     </div>
   );
