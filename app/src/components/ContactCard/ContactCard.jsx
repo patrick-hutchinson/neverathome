10167cba38c4106ceea9d59c9c2af5089ec1c73e
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-11-12 10:50:03 +0100

    Updated Role

--- app/src/components/ContactCard/ContactCard.jsx
+++ app/src/components/ContactCard/ContactCard.jsx
@@ -4,7 +4,7 @@ const ContactCard = ({ item, index, typo, onMouseEnter, onMouseLeave }) => {
   return (
     <li key={index} className={styles.contact} typo={typo} onMouseLeave={onMouseLeave} onMouseEnter={onMouseEnter}>
       {item?.name} <br />
-      Role <br />
+      {item.role} <br />
       <a href={`mailto:${item?.email}`} target="_blank">
         {item?.email}
       </a>
