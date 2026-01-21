bb8aed3828a4a342afae71ae0d09a7f830bf4080
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-01-21 15:43:05 +0100

    Updated About Page

--- app/src/components/Accordeon/AccordeonContent.jsx
+++ app/src/components/Accordeon/AccordeonContent.jsx
@@ -4,12 +4,18 @@ import Text from "../Text";
 import Gallery from "./Gallery/Gallery";
 
 import styles from "./Accordeon.module.css";
+import { usePathname } from "next/navigation";
 
 const AccordeonContent = ({ item, isExpanded, setImageInView, containerRef }) => {
+  const pathname = usePathname();
+
+  const isAbout = pathname === "/about";
   return (
     <Collapse isExpanded={isExpanded} id={item._id} containerRef={containerRef}>
       <MediaPair className={styles.mediaPair}>
-        <Text text={item.info} className={styles.description} typo="h3" />
+        <div className={`${styles.description} ${isAbout && styles.aboutPage}`}>
+          <Text text={item.info} typo="h3" />
+        </div>
         <Gallery event={item} className={styles.gallery} setImageInView={setImageInView} />
       </MediaPair>
     </Collapse>
