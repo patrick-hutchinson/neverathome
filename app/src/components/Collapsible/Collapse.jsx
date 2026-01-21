4df6022f76c9406f2315d9ee41a864f1af72c8f1
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-01-21 13:21:28 +0100

    Update Collapse.jsx

--- app/src/components/Collapsible/Collapse.jsx
+++ app/src/components/Collapsible/Collapse.jsx
@@ -9,7 +9,10 @@ const Collapse = ({ children, isExpanded, id, onScroll }) => {
     if (ref.current && isExpanded) setHeight(ref.current.scrollHeight);
   }, [children]);
 
-  const expandDuration = Math.max(height / 10000, 0.5);
+  // const expandDuration = Math.max(height / 10000, 0.5);
+  const expandDuration = 0.5;
+
+  const [allowOverflow, setAllowOverflow] = useState(false);
 
   return (
     <motion.div
@@ -23,21 +26,36 @@ const Collapse = ({ children, isExpanded, id, onScroll }) => {
         collapsed: {
           maxHeight: 0,
           opacity: 0,
-          overflow: "hidden",
+          // overflow: "hidden",
           transition: {
             maxHeight: { duration: expandDuration, delay: expandDuration, ease: "easeInOut" },
             opacity: { duration: 0.4, delay: 0, ease: "easeInOut" },
+            // overflow: { delay: 1 },
           },
         },
         expanded: {
           maxHeight: height,
           opacity: 1,
-          overflow: "visible",
+          // overflow: "visible",
           transition: {
             maxHeight: { duration: expandDuration, delay: 0, ease: "easeInOut" },
             opacity: { duration: 0.4, delay: expandDuration, ease: "easeInOut" },
+            // overflow: { delay: 1 },
           },
         },
+      }}
+      onAnimationStart={() => {
+        if (isExpanded) {
+          setAllowOverflow(true); // collapsing → lock scroll immediately
+        }
+      }}
+      onAnimationComplete={() => {
+        if (!isExpanded) {
+          setAllowOverflow(false); // expanded → allow scroll AFTER animation
+        }
+      }}
+      style={{
+        overflow: allowOverflow ? "visible" : "hidden",
       }}
     >
       {children}
