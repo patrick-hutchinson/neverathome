8d89027f4d0ac7a80869590f05900ca61cdf92f5
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-24 11:52:07 +0100

    Updated Newsletter Header

--- app/src/app/newsletter/components/NewsletterHeader.jsx
+++ app/src/app/newsletter/components/NewsletterHeader.jsx
@@ -8,8 +8,8 @@ const NewsletterHeader = ({ newsletter }) => {
       cellSpacing="0"
       style={{
         backgroundColor: "#fff",
-        padding: "12px",
-        height: "50px",
+        padding: "8px",
+        paddingBottom: "50px",
         border: "0px",
       }}
     >
@@ -18,52 +18,54 @@ const NewsletterHeader = ({ newsletter }) => {
           {/* LEFT SIDE (Desktop only) */}
           <td
             align="left"
-            valign="middle"
+            valign="bottom"
             style={{
               width: "73.5%",
               fontSize: "18px",
-              lineHeight: "20px",
+              lineHeight: "18px",
               color: "#000",
               padding: "0",
               margin: "0",
             }}
           >
             <table role="presentation" border="0" cellPadding="0" cellSpacing="0">
-              <tr>
-                <td valign="middle" style={{ paddingRight: "8px" }}>
-                  <img
-                    src="https://www.never-at-home.at/assets/newsletter/logo.gif"
-                    height="13"
-                    style={{ display: "block", border: "0" }}
-                    alt="Logo"
-                  />
-                </td>
-                <td valign="middle" style={{ fontSize: "18px", lineHeight: "20px" }}>
-                  Newsletter
-                </td>
-              </tr>
+              <tbody>
+                <tr>
+                  <td valign="bottom" style={{ paddingRight: "8px" }}>
+                    <img
+                      src="https://www.never-at-home.at/assets/newsletter/logo.gif"
+                      height="18"
+                      style={{ display: "block", height: "14px", width: "auto", border: "0" }}
+                      alt="Logo"
+                    />
+                  </td>
+                  <td valign="bottom" style={{ fontSize: "18px", lineHeight: "18px" }}>
+                    Newsletter
+                  </td>
+                </tr>
+              </tbody>
             </table>
           </td>
 
           <td
             align="left"
-            valign="top"
+            valign="bottom"
             style={{ border: "none", padding: "0px", position: "relative", width: "100%", width: "25%" }}
           >
             <table border="0" cellPadding="0" cellSpacing="0" style={{ border: "0" }}>
               <tbody>
                 <tr>
                   <td
-                    valign="top"
+                    valign="bottom"
                     className="footer-links-td newsletter-release"
                     style={{
                       fontSize: "18px",
-                      lineHeight: "20px",
+                      lineHeight: "18px",
                       textAlign: "left",
                       padding: "0",
                     }}
                   >
-                    <div style={{ color: "#000" }}>{newsletter.release}</div>
+                    <div style={{ color: "#000", lineHeight: "18px" }}>{newsletter.release}</div>
                   </td>
                 </tr>
               </tbody>
